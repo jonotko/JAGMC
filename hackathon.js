a6d61617525125914c935375839762681a87fac4
devmas <devmas@gmail.com>
2015-10-17 05:41:48 -0400

    Added hackathon.js

---
+++ hackathon.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/** Creates a user. */
+function makeUser(name, email, age, isMale, ethnicity, address) {
+    return {
+        'name':name,
+        'email':email,
+        'age':age,
+        'isMale':isMale,
+        'ethnicity':ethnicity,
+        'address':address
+    };
+}
